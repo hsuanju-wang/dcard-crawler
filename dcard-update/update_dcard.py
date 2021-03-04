881dfce587accf1e3b547f0b79bb7c3c2a310581
Phoebe <hsuanju.wang@gmail.com>
2021-03-05 02:09:21 +0800

    Delete db information

--- dcard-update/update_dcard.py
+++ dcard-update/update_dcard.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[15]:
 
 
+import argparse
+import pymysql
 import requests, json
 h= {'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'}
 import threading
@@ -14,33 +16,28 @@ import json
 import re
 
 
-# In[2]:
+# In[16]:
 
 
 # argparse
 def process_command():
     parser = argparse.ArgumentParser()
     parser.add_argument("--forum", help="forum you want" ,type = str ,default="all")
-    parser.add_argument("--start_date", help="start_date", type = str, default="-1")
-    parser.add_argument("--end_date", help="end_date" ,type = str, default="-1")
     return parser.parse_args()
 
 
-# In[3]:
+# In[17]:
 
 
-# 輸出位址
 import pymysql
 def connectToDB(forum):
     db = pymysql.connect("xxx", "xxx", "xxx", charset='utf8')
     cursor = db.cursor()
-    sql = "CREATE DATABASE IF NOT EXISTS crawler_dcard_"+forum+" DEFAULT CHARACTER SET utf8"
-    cursor.execute(sql)
     cursor.execute("use crawler_dcard_"+forum)  # 設定database
     return (cursor,db)
 
 
-# In[4]:
+# In[38]:
 
 
 # get post
@@ -63,7 +60,7 @@ def parse_post(post_id):
     return dic
 
 
-# In[15]:
+# In[39]:
 
 
 #用於scrapDcard() 中thread的function
@@ -83,45 +80,36 @@ def function(ID,forum):
     params = (result['id'],result['forumAlias'],result['artTitle'],result['artDate'],result['artUrl'],result['cleanResult'])               
     #輸入進資料庫
     try:
-       # 執行sql
+       # 执行sql语句
        cursor.execute(sql2,params)
-       # 執行sql
+       # 执行sql语句
        db.commit()
     except:
-       # 發生錯誤回傳訊息
+       # 发生错误时回滚
        db.rollback()
     cursor.close
     
 
 
-# In[11]:
+# In[40]:
 
 
 # 爬指定時段
 def scrapDcard(post_alias,startDate,endDate):
     dcard = Dcard()
-    if startDate=="-1":
-        metas = dcard.forums(post_alias).get_metas(num = -1)
-    else :
-        metas = dcard.forums(post_alias).get_metas(num = -1, timebound=startDate)
-    #取出版面所有id 
-    if endDate=="-1":
-        nowTime = time.strftime("%Y-%m-%d", time.localtime())
-        listID = [result.get('id') for result in metas if result.get('createdAt')<=nowTime]
-    else:
-        listID = [result.get('id') for result in metas if result.get('createdAt')<=endDate]
-       
-       #print(listID )
+    metas = dcard.forums(post_alias).get_metas(num = -1, timebound=startDate)
+    threadAmount = 90
+    #取出版面所有id
+    listID = [result.get('id') for result in metas if result.get('createdAt')<=endDate]
     print(listID )
-    time.sleep(10)
-    threadAmount = 85
+    
     # 用thread一次爬所有文章
     threads = []
     i = 0
     for ID in listID:  
         i+=1
         if(i%threadAmount==0):
-            time.sleep(10)
+            time.sleep(7)
         t = threading.Thread(target=function,args=[ID,post_alias])
         threads.append(t)
         t.start()
@@ -129,30 +117,60 @@ def scrapDcard(post_alias,startDate,endDate):
     # 等待所有的子執行緒結束
     for t in threads:
         t.join()
-    print("Finish crawling "+ post_alias)
-
-# In[8]:
 
 
-def scrapDcardAll(startDate,endDate):
-    scrapDcard("nsysu",startDate,endDate)
-    scrapDcard("girl",startDate,endDate)
-    scrapDcard("entertainer",startDate,endDate)
-    scrapDcard("funny",startDate,endDate)
-    scrapDcard("mood",startDate,endDate)
-    scrapDcard("trending",startDate,endDate)
-    scrapDcard("rainbow",startDate,endDate)
-    scrapDcard("relationship",startDate,endDate)
-    scrapDcard("makeup",startDate,endDate)
+# In[18]:
 
 
-# In[53]:
+def latestTime(forum): 
+    cursor,db = connectToDB(forum)
+    now = datetime.datetime.now()
+    sql = "SELECT artDate FROM "+forum+"_"+str(now.year)+" ORDER BY artDate DESC"
+    try:
+        cursor.execute(sql)
+    except:
+        sql = "SELECT artDate FROM "+forum+"_"+str(now.year-1)+" ORDER BY artDate DESC"
+        cursor.execute(sql)
+    result = cursor.fetchone()
+    cursor.close
+    #print(len(result))
+    return result
+
+
+# In[37]:
+
+
+def updateToLatest(forum):
+    latestArtTime = latestTime(forum)[0].strftime("%Y-%m-%d")
+    nowTime = time.strftime("%Y-%m-%d", time.localtime())
+    if(latestArtTime!=nowTime):
+        scrapDcard(forum,latestArtTime,nowTime)
+
+
+# In[ ]:
+
+
+def updateAllToLatest():
+    updateToLatest("nsysu")
+    updateToLatest("girl")
+    updateToLatest("entertainer")
+    updateToLatest("funny")
+    updateToLatest("mood")
+    updateToLatest("trending")
+    updateToLatest("rainbow")
+    updateToLatest("relationship")
+    updateToLatest("makeup")
+    
+
+
+# In[24]:
 
 
 if __name__ == '__main__':
+    dcard = Dcard()
     args = process_command()
-    if args.forum=="all":
-        scrapDcardAll(args.start_date,args.end_date)
+    if args.forum=="all"
+        updateAllToLatest()
     else:
-        scrapDcard(args.forum, args.start_date, args.end_date)
+        updateToLatest(args.forum)
 
